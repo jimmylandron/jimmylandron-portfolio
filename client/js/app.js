c491ad2dca5ac94fdd3c592dab1ed66c960afbc1
jimmylandron <jimmylandron@gmail.com>
2016-03-31 17:17:09 +0000

    added angular-route and started partials

---
+++ client/js/app.js
@@ -0,0 +1,11 @@
+var myApp = angular.module('myApp', ['ngRoute']);
+
+myApp.config(['$routeProvider', '$locationProvider', function($routeProvider, $locationProvider){
+    $routeProvider.
+        when('/home',
+        {templateUrl: 'partials/home.html', controller: 'homeController'});
+    $routeProvider.
+        otherwise({redirectTo: '/home'});
+        
+    $locationProvider.html5Mode({enabled: true, requireBase: false});
+}]);
