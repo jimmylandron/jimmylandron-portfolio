07c08c87151be397179ce79c297c511175ca0957
jimmylandron <jimmylandron@gmail.com>
2016-04-02 15:26:31 +0000

    added about and contact partials and controllers

--- client/js/app.js
+++ client/js/app.js
@@ -3,7 +3,13 @@ var myApp = angular.module('myApp', ['ngRoute']);
 myApp.config(['$routeProvider', '$locationProvider', function($routeProvider, $locationProvider){
     $routeProvider.
         when('/home',
-        {templateUrl: 'partials/home.html', controller: 'homeController'});
+            {templateUrl: 'partials/home.html', controller: 'homeController'});
+    $routeProvider.
+        when('/about',
+            {templateUrl: 'partials/about.html', controller: 'aboutController'});
+    $routeProvider.
+        when('/contact',
+            {templateUrl: 'partials/contact.html', controller: 'contactController'});
     $routeProvider.
         otherwise({redirectTo: '/home'});
         
