d2bb3fa7e05df752dfdb8367db9f70383451333a
jimmylandron <jimmylandron@gmail.com>
2016-04-08 16:47:13 +0000

    added carousel

--- client/js/app.js
+++ client/js/app.js
@@ -1,15 +1,15 @@
-var myApp = angular.module('myApp', ['ngRoute']);
+var myApp = angular.module('myApp', ['ngRoute', 'ui.bootstrap']);
 
 myApp.config(['$routeProvider', '$locationProvider', function($routeProvider, $locationProvider){
     $routeProvider.
         when('/home',
-            {templateUrl: 'partials/home.html', controller: 'homeController'});
+            {templateUrl: '/partials/home.html', controller: 'homeController'});
     $routeProvider.
         when('/about',
-            {templateUrl: 'partials/about.html', controller: 'aboutController'});
+            {templateUrl: '/partials/about.html', controller: 'aboutController'});
     $routeProvider.
         when('/contact',
-            {templateUrl: 'partials/contact.html', controller: 'contactController'});
+            {templateUrl: '/partials/contact.html', controller: 'contactController'});
     $routeProvider.
         otherwise({redirectTo: '/home'});
         
--- client/js/controllers/homeController.js
+++ client/js/controllers/homeController.js
@@ -1,3 +1,9 @@
 myApp.controller('homeController', ['$scope', function($scope){
+    $scope.myInterval = 4000;
+    $scope.slides = [
+        {image: "img/angular.png"},
+        {image: "img/fullstack.png"},
+        {image: "img/node.png"}
+    ];
     
 }]);
